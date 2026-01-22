b6c217773350ad951cf26c05eb11fe31efccf446
Kalpana Sharma <kanku3140@gmail.com>
2026-01-22 18:09:43 +0530

    Add left-aligned star triangle pattern using spaces

---
+++ Pattern/left_aligned_star_triangle_pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+int main(){
+  int num ;
+  cout<<"enter number : " ;
+  cin>>num;
+  for (int row = 1 ; row<=num ; row++){
+    for (int col = 1 ; col<=num ; col++){
+        if(col<=num-row)
+        cout<<" " ;
+        else cout<<"*" ;
+    }
+    cout<<endl ;
+  }
+
+
+
+
+}
