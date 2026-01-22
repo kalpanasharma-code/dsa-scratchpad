38242936c4380f5ce5ce0b966cbf74e14bbec520
Kalpana Sharma <kanku3140@gmail.com>
2026-01-22 12:10:31 +0530

    Add sequential alphabet square pattern

---
+++ Pattern/sequential_alphabet_square_pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+int main (){
+int num ;
+cout<<"enter number : " ;
+cin>> num ;
+char ch = 'A' ;
+for (int row = 1 ; row <= num ; row ++){
+    for ( int col = 1 ; col <= num ; col++){
+        cout<<ch <<" ";
+        ch++;
+    }
+    cout<<endl ;
+}
+
+
+}
