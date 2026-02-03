b97dccab6d5b03da2b5c49aaa6b89bbba15748b7
Kalpana Sharma <kanku3140@gmail.com>
2026-02-03 14:08:37 +0530

    Add integer square root using binary search

---
+++ array/sqrt_x_binary_search.cpp
@@ -0,0 +1,17 @@
+int binarySearchSqrt(int x) {
+    if (x < 2) return x;
+
+    int start = 1, end = x, ans = 0;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (mid <= x / mid) {
+            ans = mid;
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return ans;
+}
