2e82beb6d333b4a2727cbba432bee3e439bad6aa
Kalpana Sharma <kanku3140@gmail.com>
2026-02-04 21:11:01 +0530

    add peak mountain array

--- array/PeakIndexMountainArray.cpp
+++ array/PeakIndexMountainArray.cpp
@@ -1,2 +1,21 @@
-// You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
-// Return the index of the peak element.
+#include <iostream>
+using namespace std;
+
+int peakIndexInMountainArray(int arr[], int n) {
+    int start = 0;
+    int end = n - 1;
+
+    while (start < end) {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] < arr[mid + 1]) {
+            // increasing part
+            start = mid + 1;
+        } else {
+            // decreasing part or peak
+            end = mid;
+        }
+    }
+
+    return start; // peak index
+}
