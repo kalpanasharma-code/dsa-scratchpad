48df140d5fb8648cb5d3f9d39d0949bb6a6ca8df
Kalpana Sharma <kanku3140@gmail.com>
2026-02-21 13:57:29 +0530

    Add vector STL basics: size, capacity, modifiers, iterators, sorting, and searching examples

--- STL/stl_vector_intro.cpp
+++ STL/stl_vector_intro.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include <algorithm>
 using namespace std ;
 
 int main (){
@@ -59,10 +60,65 @@ int main (){
     cout<<a[i] ;
    }
 
+  // insert at a particular index 
+
+  a.insert(a.begin()+ 3 , 45 ) ; // first tell index by a.begin()+ 3 . like here 3 is index then tell value like 45 here is value 
+
+  // clear whole vector 
+
+  a.clear() ; // content will be cleared but there will empty vector in existence .
+
+  // print first and  last element of array 
+
+  cout<<v.front() ;
+  cout<<v.back() ;
+
+  // copy value of 1 vector to another 
+
+  v = a ; //  put values of a in v 
+
+  // printing array values 
+
+  vector<int> arr ;
+  arr.push_back(7) ;
+  arr.push_back(17) ;
+  arr.push_back(20) ;
+  arr.push_back(22) ;
+  arr.push_back(28) ;
+  arr.push_back(40) ; 
+
+  for(auto it = arr.begin() ; it!= arr.end() ;it++){
+   cout<<*it <<"  " ;
+  }
+ 
+  cout<<endl ;
+
+  for(auto i : arr){
+    cout<< i <<" ";
+  }
+
+
+// sort in increasing order 
+ sort(arr.begin() , arr.end()) ;
+
+// sort in decreasing order 
+ 
+sort(arr.begin() , arr.end() ,greater<int>()) ;
+
+ sort(arr.rbegin() , arr.rend()) ;
 
 
 
 
+// search in binary search 
+ 
+cout<< binary_search(arr.begin() , arr.end() ,54) ; //54 is element i m searching for . answer wil be in 1 and 0 .
+
+ // to find index 
+
+ find(arr.begin() , arr.end() , 54 ) ; // this will give itertaor of 54 not index . to know index we will 
+
+ find(arr.begin() , arr.end() , 54 ) - arr.begin() ;  
 
 
 
